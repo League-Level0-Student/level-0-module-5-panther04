9f7e47967098d02127130920fca28b67382b3e4f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-30 19:16:19 -0700

    prime

---
+++ src/prime.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class prime {
+public static void main(String[] args) {
+	String kill =JOptionPane.showInputDialog("Type in a random number");
+
+	int die = Integer.parseInt(kill);
+	
+	for (int i = 0; i <= die; i++) {
+		System.out.println(i);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
