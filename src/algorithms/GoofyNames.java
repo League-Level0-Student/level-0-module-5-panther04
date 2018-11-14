316744c8de8a98edd4add7d427bdbdc78e781a5b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-13 19:22:14 -0800

    goofy name prime

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,13 +11,24 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String mode = JOptionPane.showInputDialog("What is your name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		System.out.println(mode.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
+for (int i = 0; i < mode.length(); i++) {
+	String s = mode.substring(i, i+1);
+	
+if (i%2==0) {
+	goofyName+=s.toUpperCase();
+}
+if (i%2==1) {
+	goofyName+=s.toLowerCase();
+}
 
+}
+JOptionPane.showMessageDialog(null,"goofyName");
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
--- src/prime.java
+++ src/prime.java
@@ -1,41 +1,22 @@
 import javax.swing.JOptionPane;
 
 public class prime {
-public static void main(String[] args) {
-	String kill =JOptionPane.showInputDialog("Type in a random number");
+	public static void main(String[] args) {
+		String kill = JOptionPane.showInputDialog("Type in a random number");
 
-	int die = Integer.parseInt(kill);
-	
-	for (int i = 0; i <= die; i++) {
-		if (i%2==0) {
-			
-		
-		System.out.println(i+"even");
-	}
-		if (i%2==1) {
-			
-			System.out.println(i+"odd");
+		int die = Integer.parseInt(kill);
+		boolean sico=true;
+		for (int i = 2; i < die; i++) {
+			if (die % i == 0) {
+				sico=false;
+				System.out.println("not prime");
+				break;
+			}
+
 		}
+		if (sico) {
+			JOptionPane.showMessageDialog(null,"It is prime");
+		}
+
 	}
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
 }
