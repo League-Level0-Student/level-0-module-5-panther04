4bcb934725f6277ffcc566d0832c1bfcd970d0cf
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-15 19:16:39 -0800

    evil polygons

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -13,14 +13,22 @@ public class EvilPolygonsChallenge {
 Robot sky = new Robot();
 		//2. Set the speed to 100
 	sky.setSpeed(100);
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Blue", "Pink"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+if (colorChoice==0) {
+	sky.setPenColor(Color.RED);
+}		
+if (colorChoice==1) {
+	sky.setPenColor(Color.BLUE);
+}
+if (colorChoice==2) {
+	sky.setPenColor(Color.PINK);
+}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String dave = JOptionPane.showInputDialog("how many polygons do you want drawn");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+	
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
