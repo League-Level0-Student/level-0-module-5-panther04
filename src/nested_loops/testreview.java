d63b8872a767b2329441b6dcaf537201b33b2b19
ilabstaff <ilabstaff@172.16.116.79>
2019-03-12 19:16:46 -0700

    test review

---
+++ src/nested_loops/testreview.java
@@ -0,0 +1,18 @@
+package nested_loops;
+
+import javax.swing.JOptionPane;
+
+public class testreview {
+	public static void main(String[] args) {
+		doubleNumber(80);
+		
+	}	
+	
+	static void doubleNumber(int x) {
+		JOptionPane.showMessageDialog(null,x*2);
+		
+		
+		
+	}
+	
+}
