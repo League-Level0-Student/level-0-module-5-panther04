9370a800feb004c981946c7319fff91d46984681
ilabstaff <ilabstaff@172.16.116.81>
2019-04-16 19:13:24 -0700

    calculator

--- src/calculator.java
+++ src/calculator.java
@@ -10,8 +10,24 @@ public class calculator {
 	String rob = JOptionPane.showInputDialog("What number do u want.");
 	String beery = JOptionPane.showInputDialog("Give me another number");
 	//3. ask the user if they want to add or subtract
-	String friends = JOptionPane.showInputDialog("Do u want to add or subtract");
+	String friends = JOptionPane.showInputDialog("Do u want to add or subtract or multiply or divide");
 	//4. give them the result
+	int mind = Integer.parseInt(rob);
+	int ham = Integer.parseInt(beery);
+	if (friends.contentEquals("add")) {
+		JOptionPane.showMessageDialog(null,mind+ham);
+	}
+	if (friends.contentEquals("subtract")) {
+		JOptionPane.showMessageDialog(null,mind-ham);
+	}
+	if (friends.contentEquals("multiply")) {
+		JOptionPane.showMessageDialog(null,mind*ham);
+	}
+	if (friends.contentEquals("divide")) {
+		JOptionPane.showMessageDialog(null,mind/ham);
+	}
+	
+	
 	System.out.println(rob+beery+friends);
 	
 
