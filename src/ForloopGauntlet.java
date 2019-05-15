28e2eef0257a05eebd2d841e95d7d95b82cce0f7
ilabstaff <ilabstaff@172.16.116.81>
2019-05-14 19:17:35 -0700

    house for loop gauntlet

---
+++ src/ForloopGauntlet.java
@@ -0,0 +1,38 @@
+
+public class ForloopGauntlet {
+public static void main(String[] args) {
+//	for (int i = 0; i < 101; i++) {
+//	System.out.println(i);	
+//	}	
+	for (int g = 100; g < 1; g--) {
+		System.out.println(g);
+	}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+}
+}
--- src/Houses.java
+++ src/Houses.java
@@ -4,52 +4,57 @@ import java.util.Random;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
-public static void main(String[] args) {
-	
-	Robot noice = new Robot();
-	noice.setWindowColor(Color.BLACK);
-	noice.hide();
-	noice.setSpeed(100);
-	noice.moveTo(5, 500);
-	noice.penDown();
-	noice.move(100);
-	noice.turn(90);
-	noice.move(50);
-	noice.turn(90);
-	noice.move(100);
-	noice.turn(270);
-	noice.setPenColor(Color.green);
-	noice.move(20);
-	for (int i = 0; i < 10; i++) {
-	noice.hide();
-	noice.setSpeed(10);	
-	noice.setRandomPenColor();
-	noice.turn(-90);
-	Random walk = new Random();
-	int talk=walk.nextInt(401)+100;
-	if (talk<300) {	
-	noice.move(talk);
-	noice.turn(45);
-	noice.move(20);
-	noice.turn(90);
-	noice.move(20);
-	noice.turn(45);
-	noice.move(talk);
-	noice.setPenColor(Color.green);
-	noice.turn(270);
-	noice.move(20);
-	}else {
-		noice.turn(90);
-		noice.move(talk);
-		noice.turn(90);
-		noice.move(talk);
+	public static Robot noice;
+	public static void main(String[] args) {
+		noice = new Robot();
+		noice.setWindowColor(Color.BLACK);
+		noice.hide();
+		noice.setSpeed(50);
+		noice.moveTo(5, 500);
+		noice.penDown();
+		noice.setPenColor(Color.ORANGE);
+		noice.move(100);
 		noice.turn(90);
 		noice.move(50);
-		noice.turn(-90);
+		noice.turn(90);
+		noice.move(100);
+			drawgrass();
+
+		for (int i = 0; i < 10; i++) {
+			noice.setRandomPenColor();
+			noice.turn(-90);
+			Random walk = new Random();
+			int talk = walk.nextInt(401) + 100;
+System.out.println(talk);
+		if (talk<150) {
+			noice.penDown();
+			noice.setPenColor(Color.ORANGE);
+			noice.move(100);
+			noice.turn(90);
+			noice.move(50);
+			noice.turn(90);
+			noice.move(100);
+				drawgrass();
+		}else {
+			
+		
+				noice.move(talk);
+				noice.turn(45);
+				noice.move(20);
+				noice.turn(90);
+				noice.move(20);
+				noice.turn(45);
+				noice.move(talk);
+		
+				drawgrass();
+		}
+		}
+
+	}
+	
+	public static void drawgrass() {
+		noice.turn(270);
 		noice.setPenColor(Color.green);
 		noice.move(20);
 	}
-	}
-
-}
 }
