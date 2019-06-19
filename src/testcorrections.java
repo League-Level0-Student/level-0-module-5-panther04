8b345af9c98cd342372485e9302310e162ea4d87
ilabstaff <ilabstaff@172.16.116.69>
2019-06-18 19:26:15 -0700

    test corrections

---
+++ src/testcorrections.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class testcorrections {
+public static void main(String[] args) {
+	
+	
+ String old ="Jhon Wick";
+	 
+	 
+	 
+	int two =11;
+	
+	int three = 23;
+	
+	String four="SOMEONE";
+	
+
+	
+	for (int i = 100; i <501; i=i+2) {
+		System.out.println(i);
+	}
+	
+	String book ="Andres";
+	
+	String sketch ="pickles";
+	
+	JOptionPane.showMessageDialog(null,"Hi im "+ book + " and i hate "+ sketch);
+	
+	
+	String today = "Wednesday";
+	if (today.equals("Wednesday")) {
+		
+	}
+	else if (today.equals("tuesday")) {
+		
+	}
+	
+	Random chrome = new Random();
+	
+	int google = chrome.nextInt();
+	
+	
+	echo("echo",5);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
