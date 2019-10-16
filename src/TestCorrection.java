7eab6b6292274f7f70817120746e6e312082bb98
ilabstaff <ilabstaff@172.16.116.77>
2019-10-15 19:21:34 -0700

    test correction

---
+++ src/TestCorrection.java
@@ -0,0 +1,26 @@
+import java.awt.JobAttributes;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class TestCorrection {
+public static void main(String[] args) {
+
+
+	Random mice = new Random();
+	
+	
+	int mouse = mice.nextInt(20);
+	System.out.println(mouse);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
